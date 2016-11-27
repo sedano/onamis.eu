5ebe0026ac849483325f94640585dca90a6761be
sedano <alejandro@sedano.me>
2016-11-27 20:42:33 +0100

    Winter theme optimizations

--- gulpfile.js
+++ gulpfile.js
@@ -97,3 +97,10 @@ gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() 
     gulp.watch('*.html', browserSync.reload);
     gulp.watch('js/**/*.js', browserSync.reload);
 });
+
+// Serve task with browserSync
+gulp.task('serve', ['browserSync'], function() {
+    // Reloads the browser whenever HTML or JS files change
+    gulp.watch('*.html', browserSync.reload);
+    gulp.watch('js/**/*.js', browserSync.reload);
+});
